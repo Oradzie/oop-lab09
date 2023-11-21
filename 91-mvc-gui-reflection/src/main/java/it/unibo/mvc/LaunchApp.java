838c89d9e687293fe2d1919ad681ef3f9e6845c7
Orazio Spina <orazio.spina@studio.unibo.it>
2023-11-21 14:01:14 +0100

    Finished exercise 91

--- 91-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 91-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -1,8 +1,12 @@
 package it.unibo.mvc;
 
+import java.lang.reflect.Constructor;
+
 import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
 import it.unibo.mvc.controller.DrawNumberControllerImpl;
 import it.unibo.mvc.model.DrawNumberImpl;
+import it.unibo.mvc.view.DrawNumberStdoutView;
 import it.unibo.mvc.view.DrawNumberSwingView;
 
 /**
@@ -10,22 +14,35 @@ import it.unibo.mvc.view.DrawNumberSwingView;
  */
 public final class LaunchApp {
 
-    private LaunchApp() { }
+    private LaunchApp() {
+    }
 
     /**
      * Runs the application.
      *
      * @param args ignored
-     * @throws ClassNotFoundException if the fetches class does not exist
-     * @throws NoSuchMethodException if the 0-ary constructor do not exist
+     * @throws ClassNotFoundException    if the fetches class does not exist
+     * @throws NoSuchMethodException     if the 0-ary constructor do not exist
      * @throws InvocationTargetException if the constructor throws exceptions
-     * @throws InstantiationException if the constructor throws exceptions
-     * @throws IllegalAccessException in case of reflection issues
-     * @throws IllegalArgumentException in case of reflection issues
+     * @throws InstantiationException    if the constructor throws exceptions
+     * @throws IllegalAccessException    in case of reflection issues
+     * @throws IllegalArgumentException  in case of reflection issues
      */
-    public static void main(final String... args) {
+    public static void main(final String... args) throws Exception {
         final var model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberControllerImpl(model);
-        app.addView(new DrawNumberSwingView());
+
+        for (int i = 0; i < 3; i++) {
+            final Class<?> clSwing = Class.forName("it.unibo.mvc.view.DrawNumberSwingView");
+            final Class<?> clStdout = Class.forName("it.unibo.mvc.view.DrawNumberStdoutView");
+            final Constructor<?> cnsSwing = clSwing.getConstructor();
+            final Constructor<?> cnsStdout = clStdout.getConstructor();
+
+            final DrawNumberView newSwing = (DrawNumberView) cnsSwing.newInstance();
+            final DrawNumberView newStdout = (DrawNumberView) cnsStdout.newInstance();
+
+            app.addView(newStdout);
+            app.addView(newSwing);
+        }
     }
 }
